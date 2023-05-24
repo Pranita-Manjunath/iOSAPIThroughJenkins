28147c959155638784f18616f6d25ed87ac51915
ZMO-MAC-TestLab-02 <zmo-mac-testlab-02@ZMO-MAC-TestLab-02s-Mac-mini.local>
2023-05-24 20:10:29 +0530

    Upload it to Sharepoint

--- scripts/sharepoint_upload.ps1
+++ scripts/sharepoint_upload.ps1
@@ -4,7 +4,7 @@ param (
     [string]$source
 )
 
-write-host "$siteUrl, $target, $source"
+Write-Host "This is echo command $siteUrl, $target, $source"
 Connect-PnPOnline -Url $siteUrl -ClientId 07fcde64-8964-4a76-9c00-e18825bf79ff -ClientSecret BZcTtNmCeO0RId+efXzNkljzhbP0Ak3fcQALdK6zOcY=
 
 # Specify the path to the folder you want to upload
